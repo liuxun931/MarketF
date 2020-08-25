db3f61c4ed9932a75fdbbbd27315c2655dbd7c54
liuxun931 <liuxun931@163.com>
2020-08-25 19:52:35 +0800

    set cart forms

--- Order/urls.py
+++ Order/urls.py
@@ -8,9 +8,11 @@ from Order.views import *
 
 urlpatterns = [
     # all starts with prefix :  /Order/
+    path('', OrderTest.as_view(), name = 'test'), 
     path('', OrderList.as_view(), name = 'mainpage'), 
     # Cutomers
     path('create/', OrderCreate.as_view()),
     path('ajax_order/', Ajax_Order,),
+    
 
 ]
